6d723664bca59e7221615a2e9478e96cf86a774c
MohtashimKasmani <Mohtashimkasmani@gmail.com>
2017-03-06 00:02:10 +0500

    If-Statment

---
+++ app.js
@@ -0,0 +1,22 @@
+var a = prompt("Enter Your Percentage","Enter Here");
+
+if(a>90)
+{
+    alert("Your Grade is A and GPA is 4.0");
+}
+else if(a>80)
+{
+    alert("Your Grade is B and GPA is 3.0");
+}
+else if(a>70)
+{
+    alert("Your Grade is C and GPA is 2.0");
+}
+else if(a>60)
+{
+    alert("Your Grade is D and GPA is 1.0");
+}
+else
+{
+    alert("Fail");
+}
